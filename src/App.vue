98faa62eef5395d5ef84329bfc5fb0a1e153c53e
jpohancenik <jakub.pohancenik@cleverlance.com>
2022-02-02 10:54:41 +0100

    feat: initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <register-to-push></register-to-push>
+</template>
+
+<script setup lang="ts">
+import RegisterToPush from "./components/registerToPush.vue";
+
+</script>
+
+<style>
+html, body {
+  font-family: 'Open Sans', sans-serif;
+  font-size: 16px;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin: 0;
+}
+</style>
---
+++ src/components/registerToPush.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>registerToPush</div>
+</template>
+
+<script setup lang="ts">
+</script>
