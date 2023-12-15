8e26b4fc22281b3ec10f8cc29427004ebe754648
jpohancenik <jakub.pohancenik@cleverlance.com>
2023-12-15 16:56:58 +0100

    feat: use firebase api with token

--- src/components/registerToPush.vue
+++ src/components/registerToPush.vue
@@ -1,13 +1,15 @@
 <template>
   <div>
     <h2>Firebase integration</h2>
+    <p>Device token:</p>
+    <code>{{ deviceToken }}</code>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted } from 'vue'
+import { defineComponent, onMounted, ref } from 'vue'
 import { initializeApp } from 'firebase/app';
-import { getMessaging, getToken } from 'firebase/messaging';
+import { getMessaging, getToken, onMessage } from 'firebase/messaging';
 
 
 export default defineComponent({
@@ -18,10 +20,10 @@ export default defineComponent({
     },
   },
 
-  setup(props) {
-    onMounted(() => {
-      console.log(props, props.swr)
+  setup() {
+    const deviceToken = ref('')
 
+    onMounted(() => {
       const firebaseConfig = {
         apiKey: 'AIzaSyArFhhaSl5ek7sCiipFYFZ4wned0g_vwp0',
         authDomain: 'sazkamobil-test.firebaseapp.com',
@@ -29,27 +31,28 @@ export default defineComponent({
         storageBucket: 'sazkamobil-test.appspot.com',
         messagingSenderId: '668904320346',
         appId: '1:668904320346:web:cfd8e9af4504945b091763'
-      };
+      }
 
       const app = initializeApp(firebaseConfig)
       const messaging = getMessaging(app);
+      onMessage(messaging, (payload) => {
+        console.log('messaging', payload)
+      })
 
       getToken(messaging, {vapidKey: 'BCbyukXPL6gq6iOa7Zddxf1D73MLsZpX3SE15L5Hcqx-Bh1Iz9g0C6tkBqxG0MEgJAKr-dUIyL8HjtbT4yDBdO4'}).then((currentToken) => {
         if (currentToken) {
-          // Send the token to your server and update the UI if necessary
-          // ...
+          deviceToken.value = currentToken
         } else {
-          // Show permission request UI
           console.log('No registration token available. Request permission to generate one.');
-          // ...
         }
       }).catch((err) => {
         console.log('An error occurred while retrieving token. ', err);
-        // ...
       });
-
-      console.log(app)
     })
+
+    return {
+      deviceToken
+    }
   },
 })
 </script>
